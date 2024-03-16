945b474bfa28d977904fcdcd0845279d13377369
pubudums <ms24033350@my.sliit.lk>
2024-03-16 13:16:21 +0530

    Resolve TSMSResponse Issue

---
+++ src/main/java/com/sep/tripmanagementservice/configuration/entity/tripcategory/TripCategory.java
@@ -0,0 +1,40 @@
+package com.sep.tripmanagementservice.configuration.entity.tripcategory;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="TRIP_CATEGORIES")
+public class TripCategory {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name="uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+    @Column(name="category_name", nullable=false)
+    private String category_name;
+    @Column(name="description", nullable=false)
+    private String description;
+    @Column(name="code", nullable=false)
+    private String code;
+    @Column(name="status")
+    private boolean status;
+    @Column(name="added_at", nullable=false)
+    private LocalDateTime added_at;
+    @Column(name="removed_at", nullable=true)
+    private LocalDateTime removed_at;
+
+}
