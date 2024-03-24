ae366a44e28cbcb392a81a205375a5386689f28d
pubudums <157895089+pubudums@users.noreply.github.com>
2024-03-24 12:35:38 +0530

    Update Test.java

--- src/main/java/com/sep/tripmanagementservice/configuration/controller/entity/Test.java
+++ src/main/java/com/sep/tripmanagementservice/configuration/controller/entity/Test.java
@@ -1,4 +1,4 @@
-package com.sep.tripmanagementservice.configuration.controller.entity;
+package com.sep.tripmanagementservice.configuration.controller.entity.sysadmin;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
