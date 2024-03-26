b0975c2677e631ced4f09ce6866becfadd44b326
Charith Banneheka <charithbanneheka123@gmail.com>
2024-03-26 14:00:38 +0530

    Merge with dev

---
+++ src/main/java/com/sep/tripmanagementservice/configuration/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.sep.tripmanagementservice.configuration.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public WebMvcConfigurer corsConfigure() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(final CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200","http://travel-trek-angular-client.s3-website-ap-southeast-1.amazonaws.com")
+						.allowedMethods(HttpMethod.GET.name(),
+						HttpMethod.POST.name(),
+						HttpMethod.PUT.name(),
+						HttpMethod.DELETE.name(),
+						HttpMethod.PATCH.name())
+				.allowedHeaders(HttpHeaders.CONTENT_TYPE
+						,HttpHeaders.AUTHORIZATION);
+			}
+		};
+	}
+
+}
