c97989cfcf5c225a83a16425ee18a9068575eca4
Charith Banneheka <charithbanneheka123@gmail.com>
2024-03-29 01:17:18 +0530

    Revert "Merge branch 'develop-pubudu' into develop"
    This reverts commit 1bcdbb942eb0d82aea214faa2fd8868c0ba03603, reversing
    changes made to b18280b1857a4b26341791e902b7023f72f9f79b.

--- src/main/java/com/sep/tripmanagementservice/configuration/controller/TestController.java
+++ src/main/java/com/sep/tripmanagementservice/configuration/controller/TestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.sep.tripmanagementservice.configuration.dto.testDto;
 import com.sep.tripmanagementservice.configuration.dto.response.TSMSResponse;
 import com.sep.tripmanagementservice.configuration.entity.Test;
